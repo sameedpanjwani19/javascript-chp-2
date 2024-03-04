d301ce8cf45e32c0ab3aaf49394db757191ff817
abdulsameed19 <154329359+abdulsameed19@users.noreply.github.com>
2024-03-04 10:36:25 +0500

    chp-2

---
+++ main.js
@@ -0,0 +1,30 @@
+
+var username;
+
+var myName = "John Doe"; 
+
+
+var message;
+message = "Hello World";
+alert(message);
+
+
+var studentName = "John Doe";
+var studentAge = 20;
+var studentGender = "Male";
+var studentGrade = "A+";
+
+alert("Student Name: " + studentName);
+alert("Student Age: " + studentAge);
+alert("Student Gender: " + studentGender);
+alert("Student Grade: " + studentGrade);
+
+
+alert("Pizza\nPizz\nPiz\nPi\nP\n")
+
+
+var email = "myemail@gmail.com";
+alert("My email address is: " + email);
+
+
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
